9900208630ce298656caae34444e1f562452acf8
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-04-06 08:28:08 +0300

    init commit

---
+++ js/arrayTasks.js
@@ -0,0 +1,23 @@
+"use strict";
+console.log("Array tasks");
+
+const numberArr = [11, -25, -15, 48, 16];
+
+// 1. parasyti funkcija kuri gauna 2 argumentus ir grazina masyva kuriame yra tie du argumentai
+
+// 2. funkcija gauna objekta
+let balsai = { balsaiUz: 50, balsaiPries: 45 };
+let balsai2 = { balsaiUz: 2, balsaiPries: 33 };
+let balsai3 = { balsaiUz: 132, balsaiPries: 132 };
+// suskaiciuoti balsu skirtuma ir ji grazinti
+
+// 3 Parasyti funkcija kuri apsuka masyva is kitos puses // 2 varijantus
+// [2, 5, -3] -> [-3, 5, 2]
+
+// 4 parasyti funkcija kuri grazina masyvo paskutini el, // bent 2-3 budus
+
+// 5 parasyti funkcija kuri gauna masyva. Masyve gali buti string ir number tipo el.
+// funkcija turi paversti visus el i string tipa ir grazinti nauja masyva su pakeistai el.
+// [1, 2, "a", "b"] => ["1", "2", "a", "b"]
+// ["abc", 123, "def", 456]
+// [1, 2, 3, 17, 24, 3, "a", "123b"]
